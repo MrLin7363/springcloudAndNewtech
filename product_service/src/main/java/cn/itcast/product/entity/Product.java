d0aae28be305deae90a41a113fe89022c3d682fa
junlin_chen <m13877977363@163.com>
2021-04-05 15:34:36 +0800

    first commit

---
+++ product_service/src/main/java/cn/itcast/product/entity/Product.java
@@ -0,0 +1,29 @@
+package cn.itcast.product.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * 商品实体类
+ */
+@Data
+@Entity
+@Table(name="product")
+public class Product {
+
+	@Id
+	private Integer pid;
+	private String pname;
+	private Double market_price;
+	private Double shop_price;
+	private String image;
+	private String pdesc;
+	private Integer is_hot;
+	private Date pdate;
+	private Integer csid;
+}
